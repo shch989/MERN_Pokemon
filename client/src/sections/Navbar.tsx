7c3039256927987f8d93458d5ba7111f87af80c1
shch989 <tankpop11@gmail.com>
2023-03-17 22:49:11 +0900

    메인페이지 구조 설계

--- client/src/sections/Navbar.tsx
+++ client/src/sections/Navbar.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import pokeballIcon from '../assets/pokeball-icon.png'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 function Navbar() {
-  return <div>Navbar</div>
+  return <nav>
+    <div className="block">
+      <img src={pokeballIcon} alt="pokeball icon" />
+    </div>
+    <div className="data"></div>
+    <div className="block">
+      <GiHamburgerMenu />
+    </div>
+  </nav>
 }
 
 export default Navbar
--- client/src/sections/Wrapper.tsx
+++ client/src/sections/Wrapper.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Wrapper() {
-  return <div>Wrapper</div>
+  return <div className='content'>Wrapper</div>
 }
 
 export default Wrapper
