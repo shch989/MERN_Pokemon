5d4b5f1721793be8203b9fb15f9162dd92acf63c
shch989 <tankpop11@gmail.com>
2023-03-18 19:39:59 +0900

    navbar 디자인

--- client/src/sections/Navbar.js
+++ client/src/sections/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { GiHamburgerMenu } from 'react-icons/gi'
@@ -27,10 +27,57 @@ const NavBlock = styled.nav`
     border: 0.5px solid rgba(255, 255, 255, 0.23);
     border-top: none;
     border-bottom: none;
+    padding: 0 20rem;
+    ul {
+      display: grid;
+      grid-template-columns: repeat(5, 20%);
+      height: 100%;
+      list-style-type: none;
+      z-index: 1;
+      position: relative;
+      a {
+        text-decoration: none;
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-bottom: 1px solid transparent;
+        li {
+          text-transform: uppercase;
+          cursor: pointer;
+          font-weight: 500;
+          letter-spacing: 0.2rem;
+        }
+        .underline {
+          display: block;
+          position: absolute;
+          z-index: 0;
+          bottom: 0;
+          background-color: white;
+          width: 20%;
+          height: calc((4px) / 2);
+          mix-blend-mode: initial;
+          pointer-events: none;
+          transition: 0.5s ease-in-out;
+        }
+        &.active {
+          li {
+            color: #fff;
+          }
+          .underline {
+            width: 100%;
+            left: 0;
+            transition: 0.5s ease-in-out;
+          }
+        }
+      }
+    }
   }
 `
 
 function Navbar() {
+  const [activeIndex, setActiveIndex] = useState(0)
+
   const navigationRoutes = [
     {
       name: 'Search',
@@ -54,6 +101,10 @@ function Navbar() {
     },
   ]
 
+  const handleNavigationClick = (index) => {
+    setActiveIndex(index)
+  }
+
   return (
     <NavBlock>
       <div className="block">
@@ -62,9 +113,15 @@ function Navbar() {
       <div className="data">
         <ul>
           {navigationRoutes.map(({ name, route }, index) => {
+            const isActive = index === activeIndex
             return (
-              <Link to={route} key={index}>
+              <Link
+                to={route}
+                key={index}
+                onClick={() => handleNavigationClick(index)}
+              >
                 <li>{name}</li>
+                {isActive && <div className="underline" />}
               </Link>
             )
           })}
